c0286f0f2c0114bf7c23fd797a6f833674d2c7f1
Jacob Bates <44348983+HouseHouseHouseHouseHouseHouse@users.noreply.github.com>
2020-12-09 00:08:03 -0800

    Update formatManga.py

--- formatManga.py
+++ formatManga.py
@@ -4,34 +4,20 @@ user = 'Jet Pham'
 mangaPath = 'C:/Users/' + user + '/Desktop/' + manga
 mapPath = 'map.txt'
 
-from Format import purge
+from Format import *
 
 purge.purge(manga, mangaPath)
 
-from Format import rename
-
 rename.rename(manga, mangaPath)
-
-from Format import convert
 
 convert.convert(manga, mangaPath)
 
-from Format import landscape
-
 landscape.landscape(manga, mangaPath)
-
-from Format import map
 
 map.map(manga, mangaPath, mapPath)
 
-from Format import chaptersToPDF
-
 chaptersToPDF.chaptersToPDF(manga, mangaPath, mapPath)
 
-from Format import masterPDF
-
 masterPDF.merge_pdfs(mangaPath, mapPath)
-
-from Format import clearMap
 
 clearMap.clearMap(mapPath)
