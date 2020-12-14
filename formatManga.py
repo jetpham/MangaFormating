e8ce8b5397d3772266b036d8c4d0c60f3bd902cd
Jet Pham <55770902+jetpham@users.noreply.github.com>
2020-12-13 17:55:50 -0800

    Revert "Merge pull request #2 from HouseHouseHouseHouseHouseHouse/master"
    This reverts commit 3d82a61f84c440b2ffd62105ce0811fe657da139.

--- formatManga.py
+++ formatManga.py
@@ -3,20 +3,34 @@ manga = 'KomiNew'
 mangaPath = '%USERPROFILE%/Desktop/' + manga
 mapPath = 'map.txt'
 
-from Format import *
+from Format import purge
 
 purge.purge(manga, mangaPath)
 
+from Format import rename
+
 rename.rename(manga, mangaPath)
+
+from Format import convert
 
 convert.convert(manga, mangaPath)
 
+from Format import landscape
+
 landscape.landscape(manga, mangaPath)
+
+from Format import map
 
 map.map(manga, mangaPath, mapPath)
 
+from Format import chaptersToPDF
+
 chaptersToPDF.chaptersToPDF(manga, mangaPath, mapPath)
 
+from Format import masterPDF
+
 masterPDF.merge_pdfs(mangaPath, mapPath)
+
+from Format import clearMap
 
 clearMap.clearMap(mapPath)
